779b57ca53af8110e01b3e4536144f5fff486080
Ryan <38301142+ryan-rez@users.noreply.github.com>
2019-05-30 16:43:51 -0300

    Add files via upload

--- render.js
+++ render.js
@@ -1,48 +1,27 @@
 var firebaseConfig = {
     apiKey: "AIzaSyDDbJBz5RY5so4UoYNBIqr2nQob1ROnrKs",
-    authDomain: "firmwares-github.firebaseapp.com",
-    databaseURL: "https://firmwares-github.firebaseio.com",
+    authDomain: "firmwares.github.io",
     projectId: "firmwares-github",
-    storageBucket: "firmwares-github.appspot.com",
-    messagingSenderId: "753178353480",
     appId: "1:753178353480:web:f3e862d355bd6e38"
 };
 firebase.initializeApp(firebaseConfig);
-/*
-var app = functions.functions().httpsCallable('app');
-app({ ok: true }).then(function (result) {
-    console.log(result)
-}).catch(function (error) {
-    // Getting the Error details.
-    var code = error.code;
-    var message = error.message;
-    var details = error.details;
-    // ...
-});
-*/
-firebase.auth().signInAnonymously().catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
+
+firebase.auth().signInAnonymously().catch(error => {
+    console.log(error)
 });
 
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
         // User is signed in.
-        var isAnonymous = user.isAnonymous;
         var uid = user.uid;
         grecaptcha.ready(function () {
             grecaptcha.execute('6LflPKYUAAAAABzbRgT1-SC8lVMJaQzT6_iNh-sQ', { action: 'validate' }).then((token) => {
-                axios.post('https://us-central1-firmwares-github.cloudfunctions.net/app/authBoot', {
-                    token: token,
-                    uid: uid
-                }).then(response => {
-                    console.log(response)
+                const render = firebase.functions().httpsCallable('render')
+                render({token: token}).then(result => {
+                    console.log(result)
                 })
             });
         });
-        // ...
     } else {
         // User is signed out.
         // ...
