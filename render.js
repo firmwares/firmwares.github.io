261e725d3e0c342fda2c6c215baf836378d6b73e
Ryan <38301142+ryan-rez@users.noreply.github.com>
2019-05-30 00:01:43 -0300

    Update render.js

--- render.js
+++ render.js
@@ -33,7 +33,7 @@ firebase.auth().onAuthStateChanged(user => {
         // User is signed in.
         var isAnonymous = user.isAnonymous;
         var uid = user.uid;
-        console.log(user)
+        console.log(user.toJson())
         // ...
     } else {
         // User is signed out.
