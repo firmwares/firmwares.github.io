5d93469da9fe6f75489d3509e890883409768337
Ryan <38301142+ryan-rez@users.noreply.github.com>
2019-05-30 10:29:24 -0300

    Update render.js

--- render.js
+++ render.js
@@ -1,7 +1,10 @@
 var firebaseConfig = {
     apiKey: "AIzaSyDDbJBz5RY5so4UoYNBIqr2nQob1ROnrKs",
-    authDomain: "firmwares.github.io",
+    authDomain: "firmwares-github.firebaseapp.com",
+    databaseURL: "https://firmwares-github.firebaseio.com",
     projectId: "firmwares-github",
+    storageBucket: "firmwares-github.appspot.com",
+    messagingSenderId: "753178353480",
     appId: "1:753178353480:web:f3e862d355bd6e38"
 };
 firebase.initializeApp(firebaseConfig);
@@ -19,12 +22,17 @@ app({ ok: true }).then(function (result) {
 */
 
 firebase.auth().signInAnonymously().then(user => {
-    console.log(user)
+    if (user.additionalUserInfo.isNewUser) {
+        grecaptcha.ready(function() {
+            grecaptcha.execute('6LflPKYUAAAAABzbRgT1-SC8lVMJaQzT6_iNh-sQ', {action: 'homepage'}).then((token) => {
+               console.log(token)
+            });
+        });
+    }
 }).catch((error) => {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
-    console.log(error)
     // ...
 });
 
@@ -33,6 +41,7 @@ firebase.auth().onAuthStateChanged(user => {
         // User is signed in.
         var isAnonymous = user.isAnonymous;
         var uid = user.uid;
+        console.log(user.toJSON())
         // ...
     } else {
         // User is signed out.
